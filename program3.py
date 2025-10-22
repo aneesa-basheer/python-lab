6458baa12565fdd2d1cec07f0e7739ef0ea7c728
aneesa-basheer <aneesabashi2004@gmail.com>
2025-10-22 09:44:48 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,16 @@
+
+List1=[2,4,-6,-8,9,7]
+List2=[i for i in List1 if(i>0)]
+print (f"positive list = {List2}")
+
+list1= [1,2,3,4,5,6]
+list2= [i**2 for i in list1]
+list2
+
+word=input("Enter a string: ")
+listVowel=[i for i in word if i in 'aeiouAEIOU']
+print(f"vowels are {listVowel}")
+
+w=input("Enter any string: ")
+OrdinalVal=[ord(i) for i in w ]
+print(OrdinalVal)
